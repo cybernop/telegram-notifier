ec742f8475ddf67df6b358163cfecd50a9ae7e2e
Alexander Schulze <cibernop@gmail.com>
2020-01-20 19:37:33 +0100

    misc

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setuptools.setup(
     version='0.1.0',
     description='Python notifications for Telegramv ia REST',
     author='Alexander Schulze',
-    author_email='cibernop@gmail.com',
     packages=setuptools.find_packages(),
     install_requires=[
         'python-telegram-bot',
